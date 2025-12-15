e1cda7a140fa83423ceb7f64d99c67a58006f9fe
mohamed-sameh777 <modisameh@gmail.com>
2025-12-16 00:51:41 +0200

    car and flight model implemented

--- src/travelbooking/model/booking/Car.java
+++ src/travelbooking/model/booking/Car.java
@@ -1,4 +1,33 @@
 package model.booking;
 
+
 public class Car {
+
+    private String id;
+    private String model;
+    private double price;
+    private boolean available;
+
+    public Car(String id, String model, double price) {
+        this.id = id;
+        this.model = model;
+        this.price = price;
+        this.available = true;
+    }
+
+    public boolean checkAvailability() {
+        return available;
+    }
+
+    public void reserveCar() {
+        available = false;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getId() {
+        return id;
+    }
 }
--- src/travelbooking/model/booking/Flight.java
+++ src/travelbooking/model/booking/Flight.java
@@ -1,4 +1,35 @@
 package model.booking;
 
+
 public class Flight {
+
+    private String id;
+    private String destination;
+    private double price;
+    private int availableSeats;
+
+    public Flight(String id, String destination, double price, int availableSeats) {
+        this.id = id;
+        this.destination = destination;
+        this.price = price;
+        this.availableSeats = availableSeats;
+    }
+
+    public boolean checkAvailability() {
+        return availableSeats > 0;
+    }
+
+    public void updateAvailability() {
+        if (availableSeats > 0) {
+            availableSeats--;
+        }
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getId() {
+        return id;
+    }
 }
